6ffbef3b52b05d85374df926b0f0bb9b1c26403d
Justen Betcher <betcherj1@newpaltz.edu>
2023-03-07 21:29:11 -0500

    created exercise.ts in model so the user can start entering exercises they have done

--- client/src/model/exercise.ts
+++ client/src/model/exercise.ts
@@ -1,12 +1,70 @@
-import data from '../data/exersises.ts'
+import { ref } from "vue";
 
-export interface exercise {
-    title: string;
+
+const Exercises = ref({
+    run: null as RunWalk | null,
+    walk: null as RunWalk | null,
+    swim: null as Swim | null,
+    calistenics: null as CalistenicsWeight | null,
+    weightTraining: null as CalistenicsWeight | null,
+    hiit: null as HIIT | null
+})
+interface RunWalk {
+    unit: boolean;
+    distance: number;
     time: number;
-    distance?: number;
-    movment?: string;
+    elevation?: number;
 }
 
-export function useExercise(){
-    
+interface Swim {
+    laps: number;
+    poolSize: boolean;
+    time: number;
+    stroke: string;
 }
+
+interface CalistenicsWeight {
+    exercise: [];
+    time: number;
+}
+
+interface HIIT {
+    exercise: [];
+    totalTime: number;
+    restTime: number;
+    workTime: number;
+}
+
+
+
+export function useExercises() {
+    return Exercises;
+}
+
+export function setRun(unitPassed: boolean, distancePassed: number, timePassed: number, elevationPassed: number){
+    Exercises.value.run = {
+        unit: unitPassed,
+        distance: distancePassed,
+        time: timePassed,
+        elevation: elevationPassed
+    }
+}
+
+export function setWalk(unitPassed: boolean, distancePassed: number, timePassed: number, elevationPassed: number){
+    Exercises.value.walk = {
+        unit: unitPassed,
+        distance: distancePassed,
+        time: timePassed,
+        elevation: elevationPassed
+    }
+}
+
+export function setSwim(lapsPassed: number, timePassed: number, poolSizePassed: boolean, strokePassed: string) {
+    Exercises.value.swim = {
+        laps: lapsPassed,
+        time: timePassed,
+        poolSize: poolSizePassed,
+        stroke: strokePassed,
+    }
+}
+
