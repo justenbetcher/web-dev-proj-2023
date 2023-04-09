5d2ef4c07ab3232a9763df1d16b9729c79239c09
Justen Betcher <betcherj1@newpaltz.edu>
2023-04-09 15:21:28 -0400

    created controllers users.js that controlles the passing of information from the server to the client

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,4 +1,64 @@
 const express = require('express');
-const model = require('../models.users');
+const model = require('../models/users');
 
 const router = express.Router();
+
+router
+    .get('/', (req, res) => {
+        const list = model.getUsers();
+        const data = { data: list, total: list.length, isSuccess: true };
+        res.send(data)
+    })
+
+    .get('/search/:q', (req, res) => {
+        const name = req.params.q;
+        console.log({ name });
+        const list = model.searchUser(name);
+        const data = { data: list, total: list.length, isSuccess: true };
+        res.send(data)
+    })
+
+    .get('/:id', (req, res) => {
+        const id = +req.params.id;
+        const user = model.getUserById(id);
+        const data = { data: user, isSuccess: true };
+        res.send(data)
+    })
+
+    .post('/', (req, res) => {
+        const user = req.body;
+
+        console.log({ user });
+        console.log( req.query );
+        console.log( req.params );
+        console.log( req.headers );
+
+        model.addUser(user);
+        const data = { data: user, isSuccess: true };
+        res.send(data)
+    })
+
+    .patch('/:id', (req, res) => {
+        const user = req.body;
+        model.updateUser(user);
+        const data = { data: product, isSuccess: true };
+        res.send(data)
+    })
+
+    .delete('/:id', (req, res) => {
+        const id = +req.params.id;
+        model.deleteUser(id);
+        const data = { data: id, isSuccess: true };
+        res.send(data)
+    })
+
+module.exports = router;
+
+/*
+Ways to pass information to server:
+    1. Query String Parameters
+    2. Route Parameters
+    3. Request Parameters
+    4. Request Headers
+    5. Cookies
+*/
