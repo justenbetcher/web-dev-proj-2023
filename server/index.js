6678844d7421919a420ab85ef44ea9fb40d76fdc
Justen Betcher <betcherj1@newpaltz.edu>
2023-04-11 11:39:00 -0400

    modified server/index so that it is async code

--- server/index.js
+++ server/index.js
@@ -7,5 +7,47 @@ const app = express();
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3001; //this port is usually used for dev
 
+// Middleware
+app
+    .use(express.json())
+    .use(express.static(path.join(__dirname, '../client/dist')))
 
+    .use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        next()
+    })
+
+    // Actions
+app
+    .get('/api/v1/', (req, res) => {
+        res.send('Hello World! From Express')
+    })
+    .use('/api/v1/users', users)
+
+// Catch all
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
+
+// Error handling
+app
+    .use((err, req, res, next) => {
+        console.error(err);
+        const msg = {
+            status: err.code || 500,
+            error: err.message || 'Internal Server Error',
+            isSuccess: false
+        }
+        res.status(msg.status).json(msg)
+    })
+
+console.log('1: About to start server')
+
+app.listen(port, () => 
+    console.log(`2: Server running at http://${hostname}:${port}/`)
+);
+
+console.log('3: Asked server to start')
 
