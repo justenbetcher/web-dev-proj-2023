29ac8d66a02f6a0e131f1e631498f5b9133ef3e7
Justen Betcher <betcherj1@newpaltz.edu>
2023-04-25 17:32:39 -0400

    created mongo.js to funnle to connection to one place

---
+++ server/models/mongo.js
@@ -0,0 +1,17 @@
+const { MongoClient, ObjectId } = require('mongodb');
+
+const url = process.env.MONGO_URL ?? '';
+const DB_NAME = prcess.env.DB_NAME ?? 'chopify';
+
+const client = new MongoClient(url);
+
+async function connect() {
+    const db = await client.connect();
+    return db.db(DB_NAME);
+}
+
+module.exports = {
+    ObjectId,
+    DB_NAME,
+    connect
+}
